8ca6f57c8266864291dd4a07c43592224711cf51
sanna <sanna.makimaa@utb.ecutbildning.se>
2025-01-07 09:11:14 +0100

    Started testing

---
+++ MainApp_CSharp.Tests/Factories/ContactFactory_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Buisness.Factories;
+using Buisness.Models;
+
+namespace MainApp_CSharp.Tests.Factories;
+
+public class ContactFactory_Tests
+{
+    [Fact]
+    public void Create_ShouldReturnContactRegistrationForm()
+    {
+        // act
+        var result = ContactFactory.Create();
+
+        // assert
+        Assert.NotNull(result);
+        Assert.IsType<ContactRegistrationForm>(result);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnContactEntity_WhenContactRegistrationFormIsSupplied()
+    {
+        // arrange
+        ContactRegistrationForm contactRegistrationForm = new();
+
+        // act
+        ContactEntity result = ContactFactory.Create(contactRegistrationForm);
+
+        // assert
+        Assert.IsType<ContactEntity>(result);
+    }
+}
---
+++ MainApp_CSharp.Tests/Helpers/UniqueIdGenerator_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Buisness.Helpers;
+
+namespace MainApp_CSharp.Tests.Helpers;
+
+public class UniqueIdGenerator_Tests
+{
+    [Fact]
+    public void Generate_ShouldReturnStringOfTypeGuid()
+    {
+        // act
+        string id = UniqueIdGenerator.GenerateUniqueId();
+
+        // assert
+        Assert.False(string.IsNullOrEmpty(id));
+        Assert.True(Guid.TryParse(id, out _));
+    }
+}
